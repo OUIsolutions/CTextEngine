877230cd420f8e2e29778e9e1c453468be10b47a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-18 08:25:59 -0300

    at

---
+++ CwebEngine/tags.h
@@ -0,0 +1,9 @@
+//
+// Created by jurandi on 18-04-2023.
+//
+#define BODY "body"
+#define DIV "div"
+#define H1 "h1"
+
+
+
--- main.c
+++ main.c
@@ -1,3 +1,10 @@
 //
 // Created by jurandi on 18-04-2023.
 //
+#include <studio.h>
+
+int main(){
+
+
+    printf("aaaaaaa");
+}
