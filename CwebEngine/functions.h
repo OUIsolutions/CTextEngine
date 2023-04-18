34102086559beb3f1cf1fa438d9f5548d59632af
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-18 08:25:56 -0300

    att

---
+++ CwebEngine/functions.h
@@ -0,0 +1,8 @@
+//
+// Created by jurandi on 18-04-2023.
+//
+
+#ifndef CWEBENGINE_FUNCTIONS_H
+#define CWEBENGINE_FUNCTIONS_H
+
+#endif //CWEBENGINE_FUNCTIONS_H
---
+++ main.c
@@ -0,0 +1,3 @@
+//
+// Created by jurandi on 18-04-2023.
+//
