4a250015d0ed1321e2fb3dca590abb06f6f7a9c0
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-24 08:21:37 -0300

    at

--- conditions.c
+++ conditions.c
@@ -3,9 +3,12 @@
 
 
 
-struct CTextStack * create_html(const char *lang, const char *text){
-    
+int main(){
+    const char *lang = "en";
+    const char *text = "text exemple";
     struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
+
+
 
     s->$open(s,HTML,"lang=\"%s\"",lang);
         s->open(s,HEAD);
@@ -17,19 +20,19 @@ struct CTextStack * create_html(const char *lang, const char *text){
             s->close(s,H1);
             s->open(s,P);
                 s->segment_format(s,"This is a formated  text  %s",text);
+            s->close(s,P);
+
         s->close(s,BODY);
     s->close(s,HTML);
 
-    
-    return s;
-}
-
-
-int main(){
-
-   struct CTextStack *s = create_html("en","text");
-   printf("%s",s->rendered_text);
-
+   printf("%s\n",s->rendered_text);
+   int ident_level = s->ident_level;
+   if(ident_level == 0){
+        printf("all identation is ok\n");
+    }else{
+        printf("identation error\n");
+        printf("unclosed tags: %d\n",ident_level);
+    }
    s->free(s);
 
 }
