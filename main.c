8af72f2b07f0d401c3e176b5399300a2009d15d3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-04-24 02:29:50 -0300

    at

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
-//
-// Created by jurandi on 18-04-2023.
-//
-
-
-//#include "CTextEngine/CTextEngineMain.c"
+#include "doTheWorld.c"
 #include "CTextEngine.h"
 
 
 
-
-struct CTextStack * create_html(const char *props, int data){
+struct CTextStack * create_html(const char *lang, const char *text){
     
     struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
-    s->$open(s,HTML,"teste=\"%s\"",props);
+
+    s->text(s,"<!DOCTYPE html>");
+    s->segment(s);
+    s->$open(s,HTML,"lang=\"%s\"",lang);
+        s->open(s,HEAD);
+            
+            s->$open(s,META,"charset=\"utf-8\"");
+            s->close(s,NULL);
+            s->$open(s,META,"name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"");
+            s->close(s,NULL);
+
+            
+            s->open(s,TITLE);
+                s->text(s,"This is a title");
+            s->close(s,TITLE);
+        s->close(s,HEAD);
+
         s->open(s,BODY);
             s->open(s,H1);
-                s->open(s,NULL);
-                    s->text(s,"Hello World");
-                    s->segment(s);
-                    s->format(s, "int value: %d", data);
-                    s->segment(s);
-                    s->format(s, "string value: %s", props);
-                    s->segment(s);
-                    s->format(s, " %c %b %d %i %s", 'b', true, 33, 34, "aaa");
-                s->close(s,NULL);
+                s->text(s,"This is an Tag");
             s->close(s,H1);
+            s->open(s,P);
+                s->format(s,"This is a %s",text);
+            s->close(s,P);
         s->close(s,BODY);
-   
     s->close(s,HTML);
 
-
-
+    
     return s;
 }
 
@@ -38,8 +41,9 @@ struct CTextStack * create_html(const char *props, int data){
 
 int main(){
 
-   struct CTextStack *s = create_html("aaa",20);
-    printf("%s",s->rendered_text);
-    s->free(s);
+   struct CTextStack *s = create_html("en","test");
+   printf("%s",s->rendered_text);
+   dtw_write_string_file_content("test.html",s->rendered_text);
+   s->free(s);
 
 }
