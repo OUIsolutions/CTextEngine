57e2325ff82b68463ee9fd36b95b9d68403a0e52
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-18 16:26:48 -0300

    att

--- build.py
+++ build.py
@@ -1,20 +1,20 @@
 
-from CToolKit import *
+import CToolKit as ct
 from os import listdir,remove
 
 
 
 STARTER  = f'CTextEngine/CTextEngineMain.h'
-amalgamated_code = generate_amalgamated_code(STARTER)
+amalgamated_code = ct.generate_amalgamated_code(STARTER)
 
 with open('exemples/CTextEngine.h','w') as f:
     f.write(amalgamated_code)
 
 
-execute_test_for_folder('clang','exemples')
+ct.execute_test_for_folder('clang','exemples')
 print('all test passed')
 
-include_code_in_markdown('README.md')
+ct.include_code_in_markdown('README.md')
 
 with open('CTextEngine.h','w') as f:
     f.write(amalgamated_code)
