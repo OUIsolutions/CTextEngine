582df67df984da56bdd7b7a93b725f0054ac95b6
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 17:38:21 -0300

    att

--- main.c
+++ main.c
@@ -3,38 +3,15 @@
 
 
 
+
 int main(){
     CTextStackModule m = newCTextStackModule();
 
-    const char *lang = "en";
-    const char *text = "text exemple";
     struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
-  
-   m.$open(s,CTEXT_HTML,"lang=\"%s\"",lang);
-       m.open(s,CTEXT_HEAD);
-        
-       m.close(s,CTEXT_HEAD);
-       m.open(s,CTEXT_BODY);
-           m.open(s,CTEXT_H1);
-               m.segment_text(s,"This is a text");
-           m.close(s,CTEXT_H1);
-           m.open(s,CTEXT_P);
-               m.segment_format(s,"This is a formated  text  %s",text);
-           m.close(s,CTEXT_P);
-
-       m.close(s,CTEXT_BODY);
-   m.close(s,CTEXT_HTML);
-
+   int age = 20;
+   const char *name = "John";
+   m.format(s,"Hes name is %s, he is %i years old ",name,age);
    printf("%s\n",s->rendered_text);
-   int ident_level =s->ident_level;
-   if(ident_level == 0){
-        printf("all identation is ok\n");
-    }else{
-        printf("identation error\n");
-        printf("unclosed tags: %d\n",ident_level);
-       m.free(s);
-        return 1;
-    }
-  m.free(s);
+  //m.free(s);
 
 }
