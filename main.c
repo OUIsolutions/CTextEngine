5797e789e2cccacbb67dbce39bbf84e342589078
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 17:59:49 -0300

    att

--- main.c
+++ main.c
@@ -4,14 +4,16 @@
 
 
 
+
 int main(){
+
     CTextStackModule m = newCTextStackModule();
-
     struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
-   int age = 20;
-   const char *name = "John";
-   m.format(s,"Hes name is %s, he is %i years old ",name,age);
-   printf("%s\n",s->rendered_text);
-  //m.free(s);
+    const char *src = "img.com";
+    m.auto$close(s,CTEXT_IMG,"src=\"%s\"",src);
+    printf("%s\n",s->rendered_text);
+    m.free(s);
 
+   
+    
 }
