69fd88dfd50e1b8b26e2e9b08fd36c14bb6f1ab9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 09:44:29 -0300

    att

--- build.py
+++ build.py
@@ -13,8 +13,10 @@ with open('exemples/CTextEngine.h','w') as f:
 
 ct.execute_test_for_folder('clang','exemples')
 print('all test passed')
+'''
 
 ct.include_code_in_markdown('README.md')
 
 with open('CTextEngine.h','w') as f:
     f.write(amalgamated_code)
+'''
