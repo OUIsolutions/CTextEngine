b71e8fd1446536002872c776e6f6c834e4f5c048
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-03 04:56:17 -0300

    att

--- CTextEngine/CTextArray/CTextArray/CTextArray.h
+++ CTextEngine/CTextArray/CTextArray/CTextArray.h
@@ -1,6 +1,22 @@
 
 
 typedef struct CTextArray{
-    
+
+    CTextStack **elements;
+    bool *ownership;
     long size;
+
 }CTextArray;
+
+CTextArray * newCTextArray();
+CTextArray * CTextArray_split(CTextStack *element, char *target);
+
+void CTextArray_append_by_ownership(CTextArray *self,CTextStack *element);
+void CTextArray_append_by_copy(CTextArray *self,CTextStack *element);
+void CTextArray_append_by_reference(CTextArray *self,CTextStack *element);
+
+CTextStack * CTextArray_get_by_ownership(CTextArray *self, int position);
+CTextStack * CTextArray_get_by_copy(CTextArray *self, int position);
+CTextStack * CTextArray_get_by_reference(CTextArray *self, int position);
+
+CTextStack * CTextArray_join(CTextArray *self, char *element);
