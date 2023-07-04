fb7f0bda095a9697ed28c20ea917d9f8b3a78000
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-04 15:38:33 -0300

    att

--- exemples/string_manipulation/##format/format.c
+++ exemples/string_manipulation/##format/format.c
@@ -4,7 +4,7 @@
 int main(){
     CTextStackModule m = newCTextStackModule();
 
-    struct CTextStack *s = newCTextStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
+    struct CTextStack *s = newCTextStack("", "");
    int age = 20;
    const char *name = "John";
   m.format(s,"Hes name is %s, he is %i years old ",name,age);
