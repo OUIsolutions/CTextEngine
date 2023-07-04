a0dbfa8bc1db9dfee4b07ce7c7582888b9340949
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-04 18:08:48 -0300

    att

--- exemples/string_manipulation/##self_pop/self_pop.c
+++ exemples/string_manipulation/##self_pop/self_pop.c
@@ -1,16 +1,14 @@
 
-
-#include "CTextEngine/CTextEngineMain.h"
-//#include "../../../CTextEngine.h"
+#include "../../../CTextEngine.h"
 
 int main(){
+
     CTextStackModule m = newCTextStackModule();
     CTextStack *s = newCTextStack_string("my string");
 
     //remove the second char and third times
     m.self_pop(s,1,2);
     printf("midle:%s\n",s->rendered_text);
-
 
     m.free(s);
 
