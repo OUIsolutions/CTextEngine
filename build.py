f2d386d8c38f12f023642f9e39d38891f1a1d40c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-13 23:53:57 -0300

    att

--- build.py
+++ build.py
@@ -13,8 +13,10 @@ ZIP_NAME ='CTextEngine'
 
 
 ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
-test = ct.FolderTestPreset(folder='tests')
+test = ct.FolderTestPreset(folder='tests',side_effect_folder='tests/side_effect')
+
 test.generate_ouptut()
+'''
 test.start_test()
 ct.include_code_in_markdown('README.md',save_file=True)
 ct.generate_amalgamated_code(STARTER,OUTPUT)
@@ -22,3 +24,4 @@ ct.generate_amalgamated_code(STARTER,OUTPUT)
 create_exemples(TEST_NAME,OUTPUT)
 
 zip_folder(ZIP_NAME)
+'''
