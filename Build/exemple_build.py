d087fee4f6357bdf86f934992d15a155ae1c2c7a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-14 00:21:58 -0300

    at

--- Build/exemple_build.py
+++ Build/exemple_build.py
@@ -8,33 +8,19 @@ from os.path import isdir,dirname
 
 
 
-def move_all_c(destination:str,current_path:str,test_name:str,output:str):
-    elements = listdir(current_path)
-    for e in elements:
-        path = f'{current_path}/{e}'
-
-        if isdir(path):
-            move_all_c(destination,path,test_name,output)
-            continue
-
-        if e.endswith('.c') or e.endswith('.cpp'):
-            with open(path,'r') as arq:
-                content = arq.read()
-
-                content = content.replace(f'../../../{test_name}',output)
-                content = content.replace(f'../../{test_name}',output)
-
-            name =dirname(path).split('/')[-1].replace('T_','')
-            with open(f'{destination}/{name}.c','w') as arq2:
-                arq2.write(content)
-
-
 
 def create_exemples(test_name:str,output:str):
     rmtree('exemples',ignore_errors=True)
-    elements = listdir('tests/main_test')
+    elements = listdir('tests/')
     for e in elements:
-        path = f'tests/main_test/{e}'
-        if isdir(path):
-            dest = f'exemples/'
-            move_all_c(dest,path,test_name,output)
+        path = f'tests/{e}'
+
+        if e == 'side_effect':
+            continue
+
+        with open(f'{path}/exec.c','r') as arq:
+            content = arq.read()
+        
+        with open(f'exemples/{e.replace("T_","")}.c','w') as arq2:
+            arq2.write(content)
+            
