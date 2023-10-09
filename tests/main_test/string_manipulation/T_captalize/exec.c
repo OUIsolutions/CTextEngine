ee09f0d0276ff4996b8e44b42ae4d21ed187f21d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-10-08 22:56:52 -0300

    at

---
+++ tests/main_test/string_manipulation/T_captalize/exec.c
@@ -0,0 +1,15 @@
+#include "../../../CTextEngine.h"
+
+int main(){
+
+    CTextNamespace ctext = newCTextNamespace();
+    CTextStackModule stack = ctext.stack;
+
+    CTextStack *s = stack.newStack_string(
+            "    Test aaaa aa "
+    );
+    CTextStack *s2 =stack.captalize(s);
+    printf("%s\n",s2->rendered_text);
+    stack.free(s2);
+    stack.free(s);
+}
---
+++ tests/main_test/string_manipulation/T_self_capitalize/exec.c
@@ -0,0 +1,20 @@
+#include "../../../CTextEngine.h"
+
+int main(){
+
+    CTextNamespace ctext = newCTextNamespace();
+    CTextStackModule stack = ctext.stack;
+
+
+    CTextStack *s = stack.newStack_string(
+            " mateus moutinho queiroz"
+    );
+    stack.self_capitalize(s);
+    printf("%s",s->rendered_text);
+
+   stack.free(s);
+
+
+}//
+// Created by jurandi on 04-07-2023.
+//
