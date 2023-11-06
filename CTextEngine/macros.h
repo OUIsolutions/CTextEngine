fde06e399a2c05fa20a9d0bd1cb11fe34dc038d9
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-06 17:10:20 -0300

    at

--- CTextEngine/macros.h
+++ CTextEngine/macros.h
@@ -1,19 +1,9 @@
 
-#define CText$Scope(element,tag,args,scope) \
-        CTextStack_$open(element,tag,args,NULL);\
-        scope\
-        ctext_close(element,tag);
 
-#define CtextArgs(...)__VA_ARGS__
+#define CTextScope(s,t)\
+ctext_open(s, t);\
+for(int i = 0; i < 1; ctext_close(s, t), ++i)
 
-#define CTextScope(element,tag,scope) \
-        ctext_open(element,tag);\
-        scope\
-        ctext_close(element,tag);
-
-/*
-
-#define CTextScope(s,t) ctext_open(s, t); for(int i = 0; i < 1; ctext_close(s, t), ++i)
-
-#define $OPEN(s,t, ...) s->$open(s, t, VA_ARGS); for(int i = 0; i < 1; s->close(s, t), ++i)
- */
+#define CText$Scope(s,t, ...)\
+CTextStack_$open(s,t,__VA_ARGS__);\
+for(int i = 0; i < 1; ctext_close(s, t), ++i)
