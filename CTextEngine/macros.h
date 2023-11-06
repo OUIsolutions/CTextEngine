408578b8069e91b8979a3a6bf0a2f600d74eae46
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-11-06 15:07:49 -0300

    att

--- CTextEngine/macros.h
+++ CTextEngine/macros.h
@@ -1,5 +1,12 @@
 
-#define CTextScope(element,tag,scope,...) \
-        CTextStack_$open(element,tag,__VA_ARGS__,NULL);\
+#define CText$Scope(element,tag,args,scope) \
+        CTextStack_$open(element,tag,args,NULL);\
+        scope\
+        ctext_close(element,tag);
+
+#define CtextArgs(...)__VA_ARGS__
+
+#define CTextScope(element,tag,scope) \
+        ctext_open(element,tag);\
         scope\
         ctext_close(element,tag);
