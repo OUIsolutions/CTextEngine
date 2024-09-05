09477ecfedb68e250c911693257b774ced686dee
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-09-05 02:12:26 -0300

    ztt

--- src/declaration.h
+++ src/declaration.h
@@ -1,5 +1,9 @@
+#ifndef  CTEXTENGINE_H
 #include "imports.h"
 #include "constants/all.h"
 #include "macros/all.h"
 #include "types/all.h"
+
 #include "functions/declaration.h"
+#define  CTEXTENGINE_H
+#endif
