d4d3e131d47e049b789eab9d6eb6652d0d554e28
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-08 03:03:59 -0300

    removing test

--- tests/main_test/basic/T_free/exec.c
+++
@@ -1,8 +0,0 @@
-#include "../../../CTextEngine.h"
-int main(){
-    CTextNamespace ctext = newCTextNamespace();
-
-    struct CTextStack *s = ctext.stack.newStack(CTEXT_LINE_BREAKER, CTEXT_SEPARATOR);
-    ctext.stack.free(s);
-
-}
--- tests/main_test/scopes/T_insert_sql/expected.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO 'users' (
-    'name','email','passord'
-)
-VALUES (
-    'John','john@email.com','1234'
-    )
-
