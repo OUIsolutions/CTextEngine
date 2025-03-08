ecdf4fc2fd396810bccd52341200b795b3774247
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-08 03:46:54 -0300

    att

--- build/main.lua
+++ build/main.lua
@@ -13,7 +13,6 @@ function main()
         return 
     end 
 
-    if true then return end 
     local MAX_CONNTENT = darwin.camalgamator.ONE_MB * 20
     local MAX_RECURSION = 100
 
@@ -22,12 +21,12 @@ function main()
     local onefile = darwin.camalgamator.generate_amalgamation("src/one.c", MAX_CONNTENT, MAX_RECURSION)
     onefile = lincense .. onefile
 
-    darwin.dtw.write_file("release/BearHttpsClientOne.c", onefile)
+    darwin.dtw.write_file("release/CTextEngineOne.c", onefile)
 
     local only_declare = darwin.camalgamator.generate_amalgamation("src/imports/imports.fdeclare.h", MAX_CONNTENT,
     MAX_RECURSION)
     only_declare = lincense .. only_declare
-    darwin.dtw.write_file("release/BearHttpsClient.h", only_declare)
+    darwin.dtw.write_file("release/CTextEngine.h", only_declare)
 
 
 
@@ -41,13 +40,13 @@ function main()
         MAX_CONNTENT,
         MAX_RECURSION
     )
-    only_definition = '#include "BearHttpsClient.h"\n' .. only_definition
+    only_definition = '#include "CTextEngine.h"\n' .. only_definition
 
     only_definition = lincense .. only_definition
 
-    darwin.dtw.write_file("release/BearHttpsClient.c", only_definition)
+    darwin.dtw.write_file("release/CTextEngine.c", only_definition)
 
-    os.execute("zip -r release/BearHttpsClient.zip dependencies src build")
+    os.execute("zip -r release/CTextEngine.zip src build")
 
 
 end 
